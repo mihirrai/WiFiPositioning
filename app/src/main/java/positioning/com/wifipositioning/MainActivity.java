246af613d8eea280be9cab4bc33646455f9141a0
mihirrai <mihirrai9a@gmail.com>
2015-05-09 17:23:19 +0400

    Init

---
+++ app/src/main/java/positioning/com/wifipositioning/MainActivity.java
@@ -0,0 +1,57 @@
+package positioning.com.wifipositioning;
+
+import android.content.Context;
+import android.net.wifi.WifiManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    Button b;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b=(Button)findViewById(R.id.button);
+        b.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button:
+                WifiManager wifiManager = (WifiManager)this.getSystemService(Context.WIFI_SERVICE);
+                Log.d("Strength", wifiManager.getConnectionInfo().getRssi()+"");
+                Log.d("MAC",wifiManager.getConnectionInfo().getBSSID());
+                break;
+        }
+    }
+}
