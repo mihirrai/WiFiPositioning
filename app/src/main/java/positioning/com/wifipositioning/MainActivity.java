ddd295e056f6dc2396fadd7361d0d7e72c8b9fef
mihirrai <mihirrai9a@gmail.com>
2015-05-09 19:15:22 +0400

    delete

--- app/src/main/java/positioning/com/wifipositioning/MainActivity.java
+++ app/src/main/java/positioning/com/wifipositioning/MainActivity.java
@@ -1,7 +1,10 @@
 package positioning.com.wifipositioning;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +12,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import java.text.DecimalFormat;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener {
@@ -26,6 +32,7 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return true;
     }
 
@@ -48,10 +55,21 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.button:
-                WifiManager wifiManager = (WifiManager)this.getSystemService(Context.WIFI_SERVICE);
-                Log.d("Strength", wifiManager.getConnectionInfo().getRssi()+"");
-                Log.d("MAC",wifiManager.getConnectionInfo().getBSSID());
+                WifiManager wifiManager=(WifiManager)this.getSystemService(Context.WIFI_SERVICE);
+                List<ScanResult> results = wifiManager.getScanResults();
+                Log.d("Wifiresults", results + "");
+                /*for(ScanResult showresult: results) {
+                    Log.d("Wifiresults", results + "");
+                    DecimalFormat df = new DecimalFormat("#.##");
+                    //Log.d("Test", showresult.BSSID + ": " + showresult.level + ", d: " +
+                            //df.format(calculateDistance((double) showresult.level, showresult.frequency)) + "m");
+                }*/
                 break;
         }
+    }
+
+    public double calculateDistance(double levelInDb, double freqInMHz)    {
+        double exp = (27.55 - (20 * Math.log10(freqInMHz)) + Math.abs(levelInDb)) / 20.0;
+        return Math.pow(10.0, exp);
     }
 }
